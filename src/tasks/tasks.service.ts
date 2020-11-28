02cc9c5f0fac18672c45b5d2a3de37f8186d5655
hauchongtang <hauchongtang@users.noreply.github.com>
2020-11-28 18:31:12 +0800

    createTask

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -13,7 +13,7 @@ export class TasksService {
 
   createTask(title: string, description: string): Task {
     const task: Task = {
-      id: uuid(),
+      id: uuid.v1(),
       title,
       description,
       status: TaskStatus.OPEN,
